a53a8bea927e77938216cafbe5ca5c74dbf68d18
Elias Muellers <elias.muellers@rwth-aachen.de>
2025-11-27 22:18:10 +0100

    Added parallel end activities heuristic for frequent case

--- translucent_discovery/translucent_inductive_miner/tDFG.py
+++ translucent_discovery/translucent_inductive_miner/tDFG.py
@@ -53,6 +53,7 @@ def discover_frequent_dfg(log, subtract_xor=True, parameters={}) -> DFG:
             executed_activities.add(event["concept:name"])
     parallel = get_parallel_relationships_frequent(log, executed_activities)
     xor = get_choice_relationships_frequent(log, executed_activities)
+    added_parallel_arcs = set()
     for (source, target) in parallel:
         count = parallel[(source, target)]
         if subtract_xor:
@@ -61,8 +62,10 @@ def discover_frequent_dfg(log, subtract_xor=True, parameters={}) -> DFG:
                 xor_count = xor[(source, target)]
             if count-xor_count > 0:
                 dfg.graph.update({(source, target): count-xor_count})
+                added_parallel_arcs.add((source, target))
         else:
             dfg.graph.update({(source, target): count})
+            added_parallel_arcs.add((source, target))
     directly_follow = get_directly_follow_relationships_frequent(log, executed_activities)
     for (source, target) in directly_follow:
         count = directly_follow[(source, target)]
@@ -78,6 +81,13 @@ def discover_frequent_dfg(log, subtract_xor=True, parameters={}) -> DFG:
     for act in start_activities:
         dfg.start_activities.update(({act: start_activities[act]}))
     end_activities = get_end_activities_frequent(log, executed_activities, strict_end_activities=parameters.get("strict_end_activities", False))
+    # Heuristic: If two activities are in translucent parallel relation and one is an end activity, the other is also considered an end activity
+    if parameters.get("parallel_end_activities_heuristic", False):
+        for (source, target) in added_parallel_arcs:
+            if source in end_activities and target not in end_activities:
+                end_activities.update({target: end_activities[source]})
+            if target in end_activities and source not in end_activities:
+                end_activities.update({source: end_activities[target]})
     for act in end_activities:
         dfg.end_activities.update({act: end_activities[act]})
     return dfg
