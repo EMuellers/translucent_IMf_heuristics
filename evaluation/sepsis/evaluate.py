73a053520cff2f70e8db2587e4b991130b3e8449
Elias Muellers <elias.muellers@rwth-aachen.de>
2025-12-01 22:30:28 +0100

    renamed parameters

--- evaluation/sepsis/evaluate.py
+++ evaluation/sepsis/evaluate.py
@@ -298,7 +298,9 @@ algo_parameters = {
     
     ### PARAMETERS FOR FREQUENT ALGORITHM ###
     
-    "delta_heuristic_frequent": "before", # Signifies when to apply the remove arcs heuristic in the frequent case ("before" or "after" filtering)
+    "delta_heuristic_frequent_before": True, # Signifies if to apply the remove arcs heuristic in the frequent case on the unfiltered tDFG
+    
+    "delta_heuristic_frequent_after": False, # Signifies if to apply the remove arcs heuristic in the frequent case on the filtered tDFG
     
     ### PARAMETERS THAT APPLY TO BOTH ###
     
