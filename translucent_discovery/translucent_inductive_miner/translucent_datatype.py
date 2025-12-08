6d5768d1e209b5254028c6534e4a6f83b75d495f
Elias Muellers <elias.muellers@rwth-aachen.de>
2025-12-08 23:10:12 +0100

    bugfix

--- translucent_discovery/translucent_inductive_miner/translucent_datatype.py
+++ translucent_discovery/translucent_inductive_miner/translucent_datatype.py
@@ -33,7 +33,7 @@ def get_executed_activities(tcl: TCL) -> set[str]:
     for variant in tcl.keys():
         for event in variant:
             executed_activities.add(event[0])
-    return sorted(executed_activities)
+    return set(sorted(executed_activities))
 
 
 if __name__ == "__main__":
