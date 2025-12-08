2908f68d692a48fa5cbd7fbef3aff13ee9c39945
Elias Muellers <elias.muellers@rwth-aachen.de>
2025-12-08 14:14:05 +0100

    Added frequent tDFG discovery on TCL logs

--- translucent_discovery/translucent_inductive_miner/tDFG.py
+++ translucent_discovery/translucent_inductive_miner/tDFG.py
@@ -6,6 +6,7 @@ import pm4py
 from translucent_discovery.utils.translucent_activity_relationships import get_parallel_relationships, get_directly_follow_relationships, get_start_activities, get_end_activities
 from translucent_discovery.utils.translucent_activity_relationships import get_parallel_relationships_tcl, get_directly_follow_relationships_tcl, get_start_activities_tcl, get_end_activities_tcl
 from translucent_discovery.utils.translucent_activity_relationships import get_parallel_relationships_frequent, get_directly_follow_relationships_frequent, get_choice_relationships_frequent, get_start_activities_frequent, get_end_activities_frequent
+from translucent_discovery.utils.translucent_activity_relationships import get_parallel_relationships_frequent_tcl, get_choice_relationships_frequent_tcl, get_directly_follow_relationships_frequent_tcl, get_start_activities_frequent_tcl, get_end_activities_frequent_tcl
 from translucent_discovery.translucent_inductive_miner.translucent_datatype import TCL, get_executed_activities
 
 
@@ -133,19 +134,12 @@ def discover_frequent_dfg(log, subtract_xor=True, parameters={}) -> DFG:
     return dfg
 
 # TODO: Make this work with tcl
-def discover_frequent_dfg_tcl(log, subtract_xor=True, parameters={}) -> DFG:
-    if isinstance(log, pd.DataFrame):
-        log = log_converter.apply(log, variant=log_converter.Variants.TO_EVENT_LOG)
+def discover_frequent_dfg_tcl(log: TCL, subtract_xor=True, parameters={}) -> DFG:
     dfg = DFG()
-    executed_activities = set()
-    variants = pm4py.statistics.variants.log.get.get_variants(log) #Elias: Only used for executed activities, can stay this way
-    for variant in variants:
-        trace = variants[variant][0]
-        for event in trace:
-            executed_activities.add(event["concept:name"])
-    parallel = get_parallel_relationships_frequent(log, executed_activities)
-    xor = get_choice_relationships_frequent(log, executed_activities)
-    directly_follow = get_directly_follow_relationships_frequent(log, executed_activities)
+    executed_activities = get_executed_activities(log)
+    parallel = get_parallel_relationships_frequent_tcl(log, executed_activities)
+    xor = get_choice_relationships_frequent_tcl(log, executed_activities)
+    directly_follow = get_directly_follow_relationships_frequent_tcl(log, executed_activities)
     for (source, target) in directly_follow:
         count = directly_follow[(source, target)]
         if subtract_xor:
@@ -156,10 +150,10 @@ def discover_frequent_dfg_tcl(log, subtract_xor=True, parameters={}) -> DFG:
                 dfg.graph.update({(source, target): count - xor_count})
         else:
             dfg.graph.update({(source, target): count})
-    start_activities = get_start_activities_frequent(log, executed_activities)
+    start_activities = get_start_activities_frequent_tcl(log, executed_activities)
     for act in start_activities:
         dfg.start_activities.update(({act: start_activities[act]}))
-    end_activities = get_end_activities_frequent(log, executed_activities, strict_end_activities=parameters.get("strict_end_activities", False))
+    end_activities = get_end_activities_frequent_tcl(log, executed_activities, strict_end_activities=parameters.get("strict_end_activities", False))
     # Heuristic: If two activities are in translucent parallel relation and one is an end activity, the other is also considered an end activity
     if parameters.get("parallel_end_activities_heuristic", False):
         added_parallel_arcs = set()
