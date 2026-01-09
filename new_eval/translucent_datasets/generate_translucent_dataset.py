ba0881d953854f76a8e9a1568ef0a141bbf41eeb
Elias Muellers <elias.muellers@rwth-aachen.de>
2026-01-09 21:26:13 +0100

    Adjusted the path specification

--- new_eval/translucent_datasets/generate_translucent_dataset.py
+++ new_eval/translucent_datasets/generate_translucent_dataset.py
@@ -17,7 +17,7 @@ dataset_name='Sepsis'
 model_noise_threshold_list=[0.2, 0.4, 0.6, 0.8, 1.0] #TODO: Für n verschiedene datasets in generate_log n random samples per trace durchführen? Dann muss nicht immer wieder der DFA generiert werden.
 
 # Create output directory if it doesn't exist
-output_dir = os.path.join('new_eval', 'translucent_datasets', dataset_name)
+output_dir = os.path.join('', dataset_name)
 os.makedirs(output_dir, exist_ok=True)
 
 for model_noise_threshold in model_noise_threshold_list:
