ea7e7946ad551bc33107e521bb7c1c8f75b7e8c9
Elias Muellers <elias.muellers@rwth-aachen.de>
2026-02-10 18:33:05 +0100

    Fixed IMtf and tDFG filtering

--- translucent_discovery/translucent_inductive_miner/framework_frequent.py
+++ translucent_discovery/translucent_inductive_miner/framework_frequent.py
@@ -16,6 +16,7 @@ from copy import copy
 from copy import deepcopy
 from translucent_discovery.translucent_inductive_miner.fall_through.empty_traces import EmptyTracesTranslucent
 from translucent_discovery.translucent_inductive_miner.utils import get_delta_arcs, get_sorted_delta_arcs
+from translucent_discovery.translucent_inductive_miner.tDFG import discover_frequent_dfg_tcl
 
 T = TypeVar('T', bound=IMDataStructure)
 
@@ -76,47 +77,58 @@ class InductiveMinerFrequentFrameworkTranslucent(ABC, Generic[T]):
         if tree is None:
             # First tDFG, then filtered tDFG, then DFG, then filtered DFG
             if parameters["translucent_variant"] == "IMtf":
-                parameters["tDFG"] = True
-                cut = self.find_cut(obj, parameters=parameters)
+                if not second_iteration_normal:
+                    parameters["tDFG"] = True
+                cut = self.find_cut(obj, parameters=parameters) # called for filtered DFG and unfiltered tDFG
                 if cut is not None:
                     tree = self._recurse(cut[0], cut[1], parameters=parameters)
                 if tree is None:
-                    if not second_iteration_translucent: # Hier heuristik vor Filter
-                        if parameters.get("remove_arcs_heuristics", False) and parameters["delta_heuristic_frequent_before"]: # Apply Arc Removal Heuristics before filtering
+                    if not second_iteration_translucent: # Hier arc removal heuristik vor Filter
+                        if parameters.get("remove_arcs_heuristics", False): # Apply Arc Removal Heuristics before filtering
                                 cut = self.apply_arc_removal_heuristics(obj, parameters)
                                 if cut is not None:
                                     tree = self._recurse(cut[0], cut[1], parameters=parameters)
                         if tree is None:
                             filtered_ds = self.__filter_dfg_noise(obj, noise_threshold, True, parameters=parameters)
                             tree = self.apply(filtered_ds, parameters=parameters, second_iteration_translucent=True)
-                    if second_iteration_translucent: # Hier Heuristik nach Filter
-                        """
-                        if parameters.get("remove_arcs_heuristics", False) and parameters["delta_heuristic_frequent_after"]: # Apply Arc Removal Heuristics after filtering
-                            temp_dfg = deepcopy(obj.dfg)
-                            temp_filtered_dfg_ds = self.__filter_dfg_noise(obj, noise_threshold, False, parameters=parameters)
-                            cut = self.apply_arc_removal_heuristics(filtered_ds, parameters) # use the filtered datastructure here to use both the filtered tDFG and DFG
+                    if second_iteration_translucent: # Hier Heuristik nach Filter arc removal
+                        if parameters.get("remove_arcs_heuristics", False) and not second_iteration_normal: # Apply Arc Removal Heuristics after filtering
+                            cut = self.apply_arc_removal_heuristics(obj, parameters) 
                             if cut is not None:
                                 tree = self._recurse(cut[0], cut[1], parameters=parameters)
-                            else: # restore original dfg
-                                obj.dfg = temp_dfg
-                        """
-                        #TODO: Check if filtered_ds in IMto includes the filtered tDFG or if obj should be handed over instead!
-                        parameters["tDFG"] = False
-                        cut = self.find_cut(obj, parameters) # performed on DFG
-                        if cut is not None:
-                            parameters["tDFG"] = True
-                            tree = self._recurse(cut[0], cut[1], parameters=parameters)
-                        if tree is None:
+                        if tree is None:   # Heuristic did not yield a cut     
+                            # From here: Work on normal DFG
+                            parameters["tDFG"] = False
+                            # First we need to get the unfiltered DFG and tDFG back if it is not the second iteration
                             if not second_iteration_normal:
-                                filtered_ds = self.__filter_dfg_noise(obj, noise_threshold, False, parameters=parameters)
-                                tree = self.apply(filtered_ds, parameters=parameters, second_iteration_translucent=True, second_iteration_normal=True)
-                                if tree is None:
-                                    if parameters["tDFG_fall_through"]:
-                                        parameters["tDFG"] = True
-                                    else:
-                                        parameters["tDFG"] = False
-                                    ft = self.fall_through(obj, parameters)
-                                    tree = self._recurse(ft[0], ft[1], parameters=parameters)
+                                obj = IMDataStructureTranslucent(obj.data_structure, obj.tcl, parameters=parameters, self_loop_info = obj._translucent_self_loops)
+                                cut = self.find_cut(obj, parameters) # performed on unfiltered DFG
+                            if cut is not None:
+                                parameters["tDFG"] = True
+                                tree = self._recurse(cut[0], cut[1], parameters=parameters)
+                            if tree is None:
+                                if not second_iteration_normal:
+                                    # Apply arc addition heuristics before filtering
+                                    if parameters.get("add_arcs_heuristics", False):
+                                        cut = self.apply_arc_addition_heuristics(obj, parameters)
+                                        if cut is not None:
+                                            tree = self._recurse(cut[0], cut[1], parameters=parameters)
+                                    if tree is None: # heuristic did not yield a cut
+                                        filtered_ds = self.__filter_dfg_noise(obj, noise_threshold, False, parameters=parameters)
+                                        tree = self.apply(filtered_ds, parameters=parameters, second_iteration_translucent=True, second_iteration_normal=True) # Try filtered DFG
+                                        if tree is None:
+                                            if parameters["tDFG_fall_through"]: #TODO: Werden fallthroughs nicht sowieso auf dem log performed?
+                                                parameters["tDFG"] = True
+                                            else:
+                                                parameters["tDFG"] = False
+                                            ft = self.fall_through(obj, parameters)
+                                            tree = self._recurse(ft[0], ft[1], parameters=parameters)
+                                if second_iteration_normal: # no cut found on filtered DFG, try heuristic
+                                    # Apply arc addition heuristics on filtered DFG and filtered tDFG
+                                    if parameters.get("add_arcs_heuristics", False):
+                                        cut = self.apply_arc_addition_heuristics(obj, parameters)
+                                        if cut is not None:
+                                            tree = self._recurse(cut[0], cut[1], parameters=parameters)
             elif parameters["translucent_variant"] == "IM":
                 parameters["tDFG"] = False
                 if tree is None:
@@ -209,8 +221,9 @@ class InductiveMinerFrequentFrameworkTranslucent(ABC, Generic[T]):
     @abstractmethod
     def instance(self) -> IMInstance:
         pass
-
-
+    
+    """
+    #TODO: Shouldn't dfg and tdfg be filtered here? -> Yes!
     def __filter_dfg_noise(self, obj, noise_threshold, translucent, parameters={}):
         if translucent:
             start_activities = copy(obj.tdfg.start_activities)
@@ -242,6 +255,7 @@ class InductiveMinerFrequentFrameworkTranslucent(ABC, Generic[T]):
         }
         
         # apply filtering to end activities only if translucent
+        # TODO: Check if this is necessary
         if translucent:
             end_max_occ = max(end_activities.values())
             end_activities = {x: y for x, y in end_activities.items()
@@ -258,10 +272,94 @@ class InductiveMinerFrequentFrameworkTranslucent(ABC, Generic[T]):
 
         # Fix: Hand over the correct (t)dfg, frequent flag and parameters
         if translucent:
-            return IMDataStructureTranslucent(obj.data_structure, obj.tcl, tdfg = dfg, frequent=obj.frequent, parameters=parameters, self_loop_info = obj.__translucent_self_loops)
+            return IMDataStructureTranslucent(obj.data_structure, obj.tcl, tdfg = dfg, frequent=obj.frequent, parameters=parameters, self_loop_info = obj._translucent_self_loops)
         else:
-            return IMDataStructureTranslucent(obj.data_structure, obj.tcl, dfg = dfg, frequent=obj.frequent, parameters=parameters, self_loop_info = obj.__translucent_self_loops)
-    
+            return IMDataStructureTranslucent(obj.data_structure, obj.tcl, dfg = dfg, frequent=obj.frequent, parameters=parameters, self_loop_info = obj._translucent_self_loops)
+    """
+    def __filter_dfg_noise(self, obj, noise_threshold, translucent, parameters={}):
+        # First filter the DFG
+        start_activities = copy(obj.dfg.start_activities)
+        end_activities = copy(obj.dfg.end_activities)
+        dfg = copy(obj.dfg.graph)
+        outgoing_max_occ = {}
+        for x, y in dfg.items():
+            act = x[0]
+            if act not in outgoing_max_occ:
+                outgoing_max_occ[act] = y
+            else:
+                outgoing_max_occ[act] = max(y, outgoing_max_occ[act])
+            if act in end_activities:
+                outgoing_max_occ[act] = max(outgoing_max_occ[act], end_activities[act])
+        dfg_list = sorted([(x, y) for x, y in dfg.items()], key=lambda x: (x[1], x[0]), reverse=True)
+        dfg_list = [x for x in dfg_list if x[1] > noise_threshold * outgoing_max_occ[x[0][0]]]
+        dfg_list = [x[0] for x in dfg_list]
+        # filter the elements in the DFG
+        graph = {x: y for x, y in dfg.items() if x in dfg_list}
+        
+        # apply filtering to start activities
+        start_max_occ = max(start_activities.values())
+        start_activities = {x: y for x, y in start_activities.items()
+             if y >= start_max_occ * noise_threshold
+        }
+        
+        dfg = DFG()
+        for sa in start_activities:
+            dfg.start_activities[sa] = start_activities[sa]
+        for ea in end_activities:
+            dfg.end_activities[ea] = end_activities[ea]
+        for act in graph:
+            dfg.graph[act] = graph[act]
+        
+        # Create filtered tDFG
+        # First discover plain tfdfg
+        tfdfg = discover_frequent_dfg_tcl(obj.tcl, parameters=parameters, self_loops=obj._translucent_self_loops)
+        
+        #now apply filtering threshold to tfdfg
+        start_activities_tdfg = copy(tfdfg.start_activities)
+        end_activities_tdfg = copy(tfdfg.end_activities)
+        tdfg = copy(tfdfg.graph)
+        outgoing_max_occ_tdfg = {}
+        for x, y in tfdfg.items():
+            act = x[0]
+            if act not in outgoing_max_occ_tdfg:
+                outgoing_max_occ_tdfg[act] = y
+            else:
+                outgoing_max_occ_tdfg[act] = max(y, outgoing_max_occ_tdfg[act])
+            if act in end_activities_tdfg:
+                outgoing_max_occ_tdfg[act] = max(outgoing_max_occ_tdfg[act], end_activities_tdfg[act])
+        tdfg_list = sorted([(x, y) for x, y in tdfg.items()], key=lambda x: (x[1], x[0]), reverse=True)
+        tdfg_list = [x for x in tdfg_list if x[1] > noise_threshold * outgoing_max_occ_tdfg[x[0][0]]]
+        tdfg_list = [x[0] for x in tdfg_list]
+        # filter the elements in the DFG
+        graph_tdfg = {x: y for x, y in tdfg.items() if x in tdfg_list}
+        
+        # apply filtering to start activities
+        start_max_occ_tdfg = max(start_activities_tdfg.values())
+        start_activities_tdfg = {x: y for x, y in start_activities_tdfg.items()
+             if y >= start_max_occ_tdfg * noise_threshold
+        }
+        
+        tdfg = DFG()
+        for sa in start_activities_tdfg:
+            tdfg.start_activities[sa] = start_activities_tdfg[sa]
+        for ea in end_activities_tdfg:
+            tdfg.end_activities[ea] = end_activities_tdfg[ea]
+        for act in graph_tdfg:
+            tdfg.graph[act] = graph_tdfg[act]
+        
+        """
+        # apply filtering to end activities only if translucent
+        # TODO: Check if this is necessary
+        if translucent:
+            end_max_occ = max(end_activities.values())
+            end_activities = {x: y for x, y in end_activities.items()
+                 if y >= end_max_occ * noise_threshold
+            }
+        """
+        # Return datastructure with filtered DFG and tDFG
+        return IMDataStructureTranslucent(obj.data_structure, obj.tcl, dfg = dfg, tdfg = tdfg, frequent=True, parameters=parameters, self_loop_info = obj._translucent_self_loops)
+        
+        
     def apply_arc_removal_heuristics(self, obj: T, parameters: Optional[Dict[str, Any]] = None) -> Optional[Tuple[ProcessTree, List[T]]]:
         """Applies arc removal heuristics to try to find a cut in the modified tDFG. If no cut is found, None is returned."""
         candidate_arcs = get_delta_arcs(obj.tdfg, obj.dfg)
