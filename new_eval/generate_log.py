19d370603b1ec75a43b2237e506772d2e51ab5b5
Elias Muellers <elias.muellers@rwth-aachen.de>
2026-02-15 16:49:45 +0100

    added function to generate translucent log without noise, as noise will be handled by a different script

--- new_eval/generate_log.py
+++ new_eval/generate_log.py
@@ -99,10 +99,82 @@ def generate_log_with_noise(path_to_log, noise_threshold, alignment_parameters, 
     return annotated_log
     
 
+def generate_log_without_noise(path_to_log, noise_threshold, alignment_parameters, enabled_activities_name="enabled_activities"):
+    log = pm4py.read_xes(path_to_log, return_legacy_log_object=True)
+    
+    # First discover a ground truth model with the IMf
+    net, im, fm = pm4py.discover_petri_net_inductive(log, noise_threshold=noise_threshold)
+    
+    # Debug: Print the discovered model
+    #pm4py.view_petri_net(net, im, fm)
+    
+    rg = ReachabilityGraph(net, im, fm, 1)
+    drg = DirectReachabilityGraph(rg).dfa
+    
+    variants = pm4py.statistics.variants.log.get.get_variants(log)
+    variant_log = EventLog(attributes=log.attributes, extensions=log.extensions, classifiers=log.classifiers, omni_present=log.omni_present, properties=log.properties)
+    variant_counter = {}
+    counter = 0
+    for variant in variants:
+        variant_log.append(copy.deepcopy(variants[variant][0]))
+        variant_counter[counter] = len(variants[variant])
+        counter += 1
+    parameters = {star.Parameters.PARAM_ALIGNMENT_RESULT_IS_SYNC_PROD_AWARE: True}
+    rg = ReachabilityGraph(net, im, fm, 1)
+    drg = DirectReachabilityGraph(rg).dfa
+    global_case_id_counter = 1
+    annotated_log = EventLog(attributes=log.attributes, extensions=log.extensions, classifiers=log.classifiers, omni_present=log.omni_present, properties=log.properties)
+    
+    for index, trace in enumerate(variant_log):
+        node = frozenset({frozenset({0})}) # Indicates position in state space of the model
+        alignment_result = alignment.apply(trace, net, im, fm, parameters=parameters) # Format: (Trace / Log, Model)
+        # No sorting of alignment, as order can be argued either way. Also we can have totally different optimal alignments.
+        index_in_real_trace = 0
+        for aligned_event in alignment_result["alignment"]:
+            if aligned_event[1][1] == '>>': # Log only move
+                enabled_activities = drg.nodes[node]["enabled_activities"]
+                trace[index_in_real_trace][enabled_activities_name] = ', '.join(enabled_activities)
+                index_in_real_trace += 1
+                
+            elif aligned_event[1][0] == '>>': # Model only move
+                if aligned_event[1][1] is not None:
+                # If not tau move advance model
+                    node = get_node_from_transition(drg, node, aligned_event[0][1])
+           
+            else: # synchronous move
+                enabled_activities = drg.nodes[node]["enabled_activities"]
+                trace[index_in_real_trace][enabled_activities_name] = ', '.join(enabled_activities)
+                node = get_node_from_transition(drg, node, aligned_event[0][1])
+                index_in_real_trace += 1
+                
+        
+        counter = 0
+        while counter < variant_counter[index]:
+            trace_to_append = copy.deepcopy(trace)
+            """
+            # Introduce noise by selecting a random subset of enabled activities per event
+            for i in range(len(trace_to_append)):
+                executed_activity = trace_to_append[i]["concept:name"]
+                activities = set(trace_to_append[i][enabled_activities_name].split(', '))
+                activities.discard(executed_activity)
+                # Introduce noise by randomly removing some enabled activities
+                k = random.randint(0, len(activities))
+                noisy_activities = random.sample(list(activities), k)
+                # Ensure that the executed activity is always included (Definition of enabled activities!)
+                noisy_activities.append(executed_activity)
+                trace_to_append[i][enabled_activities_name] = ', '.join(noisy_activities)
+            """
+            trace_to_append._set_attributes({"concept:name": str(global_case_id_counter)})
+            annotated_log.append(trace_to_append)
+            counter +=1
+            global_case_id_counter +=1
+    return annotated_log
+
+
 if __name__ == "__main__":
     log_path = r"C:\\Users\\elias\\Masterarbeit_code\\Spielplatz\\Code_Harry\\TranslucentActivityRelationships-main\\evaluation\\sepsis\\Sepsis Cases - Event Log.xes.gz"
     noise_threshold = 0.8  # Example noise threshold
     alignment_params = {}
     #generate_log_with_noise(log_path, noise_threshold, alignment_params)
-    for noise_threshold in [0.2, 0.4, 0.6, 0.8, 1.0]: 
-        generate_log_with_noise(log_path, noise_threshold, alignment_params)
+    #for noise_threshold in [0.2, 0.4, 0.6, 0.8, 1.0]: 
+    generate_log_without_noise(log_path, noise_threshold, alignment_params)
