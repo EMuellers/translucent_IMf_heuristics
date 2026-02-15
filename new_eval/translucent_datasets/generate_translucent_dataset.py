1575b495ddb20afda94fc63a36063b5760221cc4
Elias Muellers <elias.muellers@rwth-aachen.de>
2026-02-15 18:18:02 +0100

    no noise used

--- new_eval/translucent_datasets/generate_translucent_dataset.py
+++ new_eval/translucent_datasets/generate_translucent_dataset.py
@@ -8,24 +8,27 @@ sys.path.append(r"C:\Users\elias\Masterarbeit_code\Spielplatz\Code_Harry\Translu
 
 import pm4py
 
-from new_eval.generate_log import generate_log_with_noise
+from new_eval.generate_log import generate_log_without_noise
 
 start = time.time()
 
 dataset_path=r"C:\\Users\\elias\\Masterarbeit_code\\Spielplatz\\Code_Harry\\TranslucentActivityRelationships-main\\evaluation\\sepsis\\Sepsis Cases - Event Log.xes.gz"
 dataset_name='Sepsis'
-model_noise_threshold_list=[0.2, 0.4, 0.6, 0.8, 1.0] #TODO: Für n verschiedene datasets in generate_log n random samples per trace durchführen? Dann muss nicht immer wieder der DFA generiert werden.
+model_noise_threshold_list=[0.2]
 
 # Create output directory if it doesn't exist
 output_dir = os.path.join('', dataset_name)
 os.makedirs(output_dir, exist_ok=True)
 
 for model_noise_threshold in model_noise_threshold_list:
-    annotated_log = generate_log_with_noise(dataset_path,
+    annotated_log = generate_log_without_noise(dataset_path,
                                             model_noise_threshold,
                                             {},
                                             enabled_activities_name="enabled_activities")
-    output_log_path = os.path.join(output_dir, f'{dataset_name}_{model_noise_threshold}.xes')
-    pm4py.write_xes(annotated_log, output_log_path)
+    #output_log_path = os.path.join(output_dir, f'{dataset_name}_{model_noise_threshold}.xes')
+    #pm4py.write_xes(annotated_log, output_log_path)
+    output_log_path = os.path.join(output_dir, f'{dataset_name}_{model_noise_threshold}.csv')
+    df_log = pm4py.convert_to_dataframe(annotated_log)
+    df_log.to_csv(output_log_path, index=False)
     print(f'Generated translucent log with noise threshold {model_noise_threshold} at {output_log_path}')
     print(f'Time taken so far: {time.time() - start} seconds')
