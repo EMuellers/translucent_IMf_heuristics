c659ef8bcd837e6bf0aa5dca67e5e97a90a4df8e
Elias Muellers <elias.muellers@rwth-aachen.de>
2026-02-22 17:27:52 +0100

    Cleaned up the code and added generation of noisy datasets

--- new_eval/translucent_datasets/generate_translucent_dataset.py
+++ new_eval/translucent_datasets/generate_translucent_dataset.py
@@ -1,28 +1,24 @@
 # Script generates translucent logs with noise with different filter paramters
 
 import os
-import time
+import copy
 import sys
-#sys.path.append(r"C:\Users\elias\Masterarbeit_code\Spielplatz\Code_Harry\TranslucentActivityRelationships-main")
-sys.path.append("/home/eliasmullers/Desktop/thesis/TranslucentActivityRelationships-main")
+if os.name == 'nt': # Windows
+    sys.path.append(r"C:\Users\elias\Masterarbeit_code\Spielplatz\Code_Harry\TranslucentActivityRelationships-main")
+else: # Linux
+    sys.path.append("/home/eliasmullers/Desktop/thesis/TranslucentActivityRelationships-main")
 
 import pm4py
 
 from new_eval.generate_log import generate_log_without_noise
+from new_eval.translucent_datasets.generate_noisy_datasets import get_noisy_log
 
-start = time.time()
 
-#dataset_path=r"C:\Users\elias\Masterarbeit_code\Spielplatz\Code_Harry\TranslucentActivityRelationships-main\new_eval\original_datasets\Hospital Billing - Event Log.xes.gz"
-#dataset_path="/home/eliasmullers/Desktop/thesis/TranslucentActivityRelationships-main/new_eval/original_datasets/Hospital Billing - Event Log.xes.gz"
-#dataset_name='hospital_billing'
-#dataset_path=r"C:\Users\elias\Masterarbeit_code\Spielplatz\Code_Harry\TranslucentActivityRelationships-main\new_eval\original_datasets\Hospital Billing - Event Log.xes.gz"
-dataset_path=r"C:\Users\elias\Masterarbeit_code\Spielplatz\Code_Harry\TranslucentActivityRelationships-main\new_eval\original_datasets\Road_Traffic_Fine_Management_Process.xes.gz"
-dataset_name='Road_Traffic_Fine_Management_Process'
+dataset_path="/home/eliasmullers/Desktop/thesis/TranslucentActivityRelationships-main/new_eval/original_datasets/Sepsis Cases - Event Log.xes.gz"
+dataset_name='Sepsis'
 model_noise_threshold_list=[0.2]
+noise_types = ["add_enabled", "add_events", "remove_enabled", "change_events"]
 
-# Create output directory if it doesn't exist
-#output_dir = os.path.join('', dataset_name)
-#os.makedirs(output_dir, exist_ok=True)
 
 for model_noise_threshold in model_noise_threshold_list:
     annotated_log = generate_log_without_noise(dataset_path,
@@ -39,13 +35,17 @@ for model_noise_threshold in model_noise_threshold_list:
             if executed_activity not in enabled_activities:
                 print(f"Error: Executed activity '{executed_activity}' is not in the set of enabled activities {enabled_activities} for event {i} in trace {trace.attributes['concept:name']}")
     
+    # Write the noisy logs
+    for noise_type in noise_types:
+        noisy_log = get_noisy_log(copy.deepcopy(annotated_log), noise_type)
+        output_log_path = f"/home/eliasmullers/Desktop/thesis/TranslucentActivityRelationships-main/new_eval/translucent_datasets/{dataset_name}/{dataset_name}_{model_noise_threshold}_{noise_type}.csv"
+        df_log = pm4py.convert_to_dataframe(noisy_log)
+        df_log.to_csv(output_log_path, index=False)
+        print(f'Generated translucent log with noise type {noise_type} and noise threshold {model_noise_threshold} at {output_log_path}')
+
     
-    #output_log_path = os.path.join(output_dir, f'{dataset_name}_{model_noise_threshold}.xes')
-    #pm4py.write_xes(annotated_log, output_log_path)
-    #output_log_path = os.path.join(output_dir, f'{dataset_name}_{model_noise_threshold}.csv')
-    output_log_path = "/home/eliasmullers/Desktop/thesis/TranslucentActivityRelationships-main/new_eval/translucent_datasets/hospital_billing/hospital_billing_0.2.csv"
-    output_log_path = r"C:\Users\elias\Masterarbeit_code\Spielplatz\Code_Harry\TranslucentActivityRelationships-main\new_eval\translucent_datasets\road_traffic_fine\road_traffic_fine_0.2.csv"
+    # Write the log without noise
+    output_log_path = f"/home/eliasmullers/Desktop/thesis/TranslucentActivityRelationships-main/new_eval/translucent_datasets/{dataset_name}/{dataset_name}_{model_noise_threshold}.csv"
     df_log = pm4py.convert_to_dataframe(annotated_log)
     df_log.to_csv(output_log_path, index=False)
     print(f'Generated translucent log with noise threshold {model_noise_threshold} at {output_log_path}')
-    print(f'Time taken so far: {time.time() - start} seconds')
