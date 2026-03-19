9455f981b3799f432349c578ace271695db5cc0d
Elias Muellers <elias.muellers@rwth-aachen.de>
2026-03-19 11:36:20 +0100

    chnages to main

--- new_eval/translucent_datasets/generate_translucent_dataset.py
+++ new_eval/translucent_datasets/generate_translucent_dataset.py
@@ -68,55 +68,59 @@ def translucent_variant_ratio(
     return traces.nunique() / len(traces)
 
 if __name__ == "__main__":
-
-    dataset_path="/home/eliasmullers/Desktop/thesis/TranslucentActivityRelationships-main/new_eval/original_datasets/Sepsis Cases - Event Log.xes.gz"
+    dataset_paths = []
+    dataset_paths.append(("/home/eliasmullers/Desktop/thesis/TranslucentActivityRelationships-main/new_eval/original_datasets/Sepsis Cases - Event Log.xes.gz",'Sepsis'))
+    dataset_paths.append(("/home/eliasmullers/Desktop/thesis/TranslucentActivityRelationships-main/new_eval/original_datasets/Road_Traffic_Fine_Management_Process.xes.gz", 'road_traffic_fine'))
+    dataset_paths.append(("/home/eliasmullers/Desktop/thesis/TranslucentActivityRelationships-main/new_eval/original_datasets/Hospital Billing - Event Log.xes.gz", 'hospital_billing'))
     if os.name == 'nt': # Windows
         dataset_path = r"C:\Users\elias\Masterarbeit_code\Spielplatz\Code_Harry\TranslucentActivityRelationships-main\new_eval\original_datasets\Hospital Billing - Event Log.xes.gz"
-    dataset_name='hospital_billing'
-    input_log_path = "/home/eliasmullers/Desktop/thesis/TranslucentActivityRelationships-main/new_eval/translucent_datasets/road_traffic_fine/road_traffic_fine_0.2.csv"
-    model_noise_threshold_list=[0.2]
+    #dataset_names=['Sepsis', 'road_traffic_fine', 'hospital_billing']
+    input_log_path = False
+    model_noise_threshold_list=[0.4]
     noise_types = ["add_enabled", "remove_enabled", "change_events"]
 
-
-    for model_noise_threshold in model_noise_threshold_list:
-        if not input_log_path:
-            annotated_log = generate_log_without_noise(dataset_path,
-                                                    model_noise_threshold,
-                                                    {},
-                                                    enabled_activities_name="enabled_activities")
+    for dataset_path, dataset_name in dataset_paths:
+        for model_noise_threshold in model_noise_threshold_list:
+            if not input_log_path:
+                pnml_path = f"/home/eliasmullers/Desktop/thesis/TranslucentActivityRelationships-main/new_eval/translucent_datasets/{dataset_name}/{dataset_name}_{model_noise_threshold}_ground_truth.pnml"
+                annotated_log = generate_log_without_noise(dataset_path,
+                                                        model_noise_threshold,
+                                                        {},
+                                                        enabled_activities_name="enabled_activities", 
+                                                        pnml_path=pnml_path)
+                
+                # Check correctness: Is the excecuted activity always included in the enabled activities?
+                for trace in annotated_log:
+                    for i in range(len(trace)):
+                        event = trace[i]
+                        executed_activity = event["concept:name"]
+                        enabled_activities = set(event["enabled_activities"].split(', '))
+                        if executed_activity not in enabled_activities:
+                            print(f"Error: Executed activity '{executed_activity}' is not in the set of enabled activities {enabled_activities} for event {i} in trace {trace.attributes['concept:name']}")
+                
+                
+            # Write the log without noise
+                output_log_path = f"/home/eliasmullers/Desktop/thesis/TranslucentActivityRelationships-main/new_eval/translucent_datasets/{dataset_name}/{dataset_name}_{model_noise_threshold}.csv"
+                if os.name == 'nt': # Windows
+                    output_log_path = f"C:\\Users\\elias\\Masterarbeit_code\\Spielplatz\\Code_Harry\\TranslucentActivityRelationships-main\\new_eval\\translucent_datasets\\{dataset_name}\\{dataset_name}_{model_noise_threshold}.csv"
+                df_log = pm4py.convert_to_dataframe(annotated_log)
+                df_log.to_csv(output_log_path, index=False)
+                print(f'Generated translucent log with noise threshold {model_noise_threshold} at {output_log_path}')
             
-            # Check correctness: Is the excecuted activity always included in the enabled activities?
-            for trace in annotated_log:
-                for i in range(len(trace)):
-                    event = trace[i]
-                    executed_activity = event["concept:name"]
-                    enabled_activities = set(event["enabled_activities"].split(', '))
-                    if executed_activity not in enabled_activities:
-                        print(f"Error: Executed activity '{executed_activity}' is not in the set of enabled activities {enabled_activities} for event {i} in trace {trace.attributes['concept:name']}")
+            else:
+                df_log = pd.read_csv(input_log_path)
             
-            
-        # Write the log without noise
-            output_log_path = f"/home/eliasmullers/Desktop/thesis/TranslucentActivityRelationships-main/new_eval/translucent_datasets/{dataset_name}/{dataset_name}_{model_noise_threshold}.csv"
-            if os.name == 'nt': # Windows
-                output_log_path = f"C:\\Users\\elias\\Masterarbeit_code\\Spielplatz\\Code_Harry\\TranslucentActivityRelationships-main\\new_eval\\translucent_datasets\\{dataset_name}\\{dataset_name}_{model_noise_threshold}.csv"
-            df_log = pm4py.convert_to_dataframe(annotated_log)
-            df_log.to_csv(output_log_path, index=False)
-            print(f'Generated translucent log with noise threshold {model_noise_threshold} at {output_log_path}')
-        
-        else:
-            df_log = pd.read_csv(input_log_path)
-        
-        # Write the noisy logs
-        for noise_type in noise_types:
-            noisy_log = get_noisy_log(copy.deepcopy(df_log), noise_type, selection_percentage=0.1)
-            output_log_path = f"/home/eliasmullers/Desktop/thesis/TranslucentActivityRelationships-main/new_eval/translucent_datasets/{dataset_name}/{dataset_name}_{model_noise_threshold}_{noise_type}.csv"
-            if os.name == 'nt': # Windows
-                output_log_path = f"C:\\Users\\elias\\Masterarbeit_code\\Spielplatz\\Code_Harry\\TranslucentActivityRelationships-main\\new_eval\\translucent_datasets\\{dataset_name}\\{dataset_name}_{model_noise_threshold}_{noise_type}.csv"
-            noisy_log.to_csv(output_log_path, index=False)
-            print(f'Generated translucent log with noise type {noise_type} and noise threshold {model_noise_threshold} at {output_log_path}')
-            print(f'Variant ratio of the noisy log: {variant_ratio(noisy_log)}')
-            print(f'Translucent variant ratio of the noisy log: {translucent_variant_ratio(noisy_log)}')
-            print()
+            # Write the noisy logs
+            for noise_type in noise_types:
+                noisy_log = get_noisy_log(copy.deepcopy(df_log), noise_type, selection_percentage=0.1)
+                output_log_path = f"/home/eliasmullers/Desktop/thesis/TranslucentActivityRelationships-main/new_eval/translucent_datasets/{dataset_name}/{dataset_name}_{model_noise_threshold}_{noise_type}.csv"
+                if os.name == 'nt': # Windows
+                    output_log_path = f"C:\\Users\\elias\\Masterarbeit_code\\Spielplatz\\Code_Harry\\TranslucentActivityRelationships-main\\new_eval\\translucent_datasets\\{dataset_name}\\{dataset_name}_{model_noise_threshold}_{noise_type}.csv"
+                noisy_log.to_csv(output_log_path, index=False)
+                print(f'Generated translucent log with noise type {noise_type} and noise threshold {model_noise_threshold} at {output_log_path}')
+                print(f'Variant ratio of the noisy log: {variant_ratio(noisy_log)}')
+                print(f'Translucent variant ratio of the noisy log: {translucent_variant_ratio(noisy_log)}')
+                print()
 
 
     
